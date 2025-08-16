d06ab48c4960291ea81053351ecdef53862ebd0c
Mayank Balayan <116083399+mayankbalayan@users.noreply.github.com>
2025-08-16 11:41:03 +0000

    stackheapsumm

--- 01_basics/05_datatype-summary.js
+++ 01_basics/05_datatype-summary.js
@@ -28,3 +28,31 @@ console.log(typeof score);
 
 // studypart
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// stack (primitive) // original value copy is made , heap (non-primitive) // reference of original value 
+
+let myyoutubename = "mayankbalayandotcom"
+
+let anothername = myyoutubename
+anothername = "Balayan"
+
+console.log(myyoutubename);// copies are made in stack original value is same
+console.log(anothername);
+
+// object store in heap and reference is provided 
+let userOne = {
+    email : "user@google.com",
+    upi : "user@ibl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "balayan@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
