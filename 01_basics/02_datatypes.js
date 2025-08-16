91840269a3d7255d00e6efa0056568629146810f
Mayank Balayan <116083399+mayankbalayan@users.noreply.github.com>
2025-08-16 09:54:16 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version 
+
+// alert(6+8) // we are using node js not the browser 
+
+console.log(3+5) // code redealibity should be high 
+
+console.log("balayan")
+
+let name = "mayank"
+let age = 18
+let islogedIn = true
+
+// number => 2 to power 53 
+// bigint => if number is greater it is used 
+// string => " "(prefered) ' '
+// boolean => true or false 
+// null => standalone value 
+// undefined => value not assigned yet 
+// symbol => used for unique ness
+// object =>
+    console.log(typeof null); // object type 
+    console.log(typeof undefined); // undefined 
+    
