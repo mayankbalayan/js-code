2335bb9a9f7621eef70aeb4079048e5d88c3956d
Mayank Balayan <116083399+mayankbalayan@users.noreply.github.com>
2025-08-16 10:21:09 +0000

    conversionOperations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,65 @@
+let score = "33abc" 
+
+// console.log(typeof score);
+
+let valueInnumber = Number(score)
+// console.log(typeof valueInnumber);
+// console.log(valueInnumber);
+// if value is string and converted into number then its converted but when we print it prints NaN(not a number)
+
+// "33" => 33
+// "33abc" => NaN
+// "true" => 1
+
+let islogedIn = "mayank";
+
+let booleanislogedin = Boolean(islogedIn)
+// console.log(booleanislogedin);
+
+// "1" = true
+// "" = false
+// "mayank" = true
+
+let someNumber = 33
+
+let stringnumber = String(someNumber)
+
+// console.log(stringnumber);
+// console.log(typeof stringnumber);
+
+
+// *********** operations ************** 
+
+let value = 33
+let negvalue = -value
+// console.log(negvalue); // -33
+
+// console.log(3+3); // arthematic operation use +,-,*,/, **(power) etc
+
+let str1 = "hello"
+let str2 = " mayank"
+let str3 = str1+str2 // combination of two string 
+
+//console.log("1" + 2);// 12
+//console.log(1+"2"); // 12
+//console.log("1"+2+2); // 122
+//console.log(2+2+"3"); // 43
+
+// always use brackets for such operations 
+
+
+// link to study 
+// https://tc39.es/ecma262/#sec-type-conversion
+
+// link to study prefix and post fix 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+
+
+
+
+
+
+
+
+
